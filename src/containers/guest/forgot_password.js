b7c26e89637104f86bf1ee94128f410277459131
miphamj <hami9696@gmail.com>
2019-07-22 09:13:22 +0700

    css

--- src/containers/guest/forgot_password.js
+++ src/containers/guest/forgot_password.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import 'antd/dist/antd.css';
 import './login.scss';
-import { Form, Icon, Input, Button, Checkbox, notification } from 'antd';
+
+import { Form, Icon, Input, Button, Checkbox, notification, Col, Row } from 'antd';
 import FormItem from 'antd/lib/form/FormItem';
-const HeadImg = require('./../../../src/assets/itleadpro.png');
+
+const HeadImg = require('../../assets/itleadpro.png');
 class Change extends React.Component {
     handleSubmit = e => {
         e.preventDefault();
@@ -26,36 +29,45 @@ class Change extends React.Component {
             });          
           };
         return (
-            <Form onSubmit={this.handleSubmit} className="login-form">
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <img src={HeadImg} width={230} />
-                </div>
-                <h1>Change Password</h1>
-                <Form.Item>
-                <div className="label">Your email</div>
-                {getFieldDecorator('username', {
-                    rules: [
-                        {
-                            required: true,
-                            message: 'Please input your email'
-                        },
-                        {
-                            type: 'email',
-                            message: 'Invalid email'
-                        }
-                    ],
-                })(
-                    <Input
-                        prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    />,
-                )}
-            </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button" onClick={() => openNotification('success')}>
-                        Change Password
-                    </Button>
-                </Form.Item>
-            </Form>
+            <Row className="wrapper">
+                <Col xs={2} md={8}></Col>
+                <Col xs={20} md={8}>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <img src={HeadImg}/>
+                        </div>
+                        <h1>Forgot Password</h1>
+                        <Form.Item>
+                        <div className="label">Your email</div>
+                        {getFieldDecorator('username', {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: 'Please input your email'
+                                },
+                                {
+                                    type: 'email',
+                                    message: 'Invalid email'
+                                }
+                            ],
+                        })(
+                            <Input
+                                prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            />,
+                        )}
+                    </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button" onClick={() => openNotification('success')}>
+                                Request reset link
+                            </Button>
+                        </Form.Item>
+                        <div style={{textAlign: 'center'}}>
+                            <a href="/login">Back to Log in</a>
+                        </div>
+                    </Form>
+                </Col>
+                <Col xs={2} md={8}></Col>
+            </Row>
         )
     }
 }
