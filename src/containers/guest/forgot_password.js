88300ebec84695e06914a381eb3e5f9a7e7c573a
unknown <hoangphong99174>
2019-07-24 10:17:30 +0700

    ff

--- src/containers/guest/forgot_password.js
+++ src/containers/guest/forgot_password.js
@@ -19,12 +19,12 @@ class Change extends React.Component {
         const { getFieldDecorator } = this.props.form;
         const openNotification = type => {
             notification[type]({
-              message: 'Change Password Successfully.',
-              description:
-                'Done. You will be redirected to login screen now.',
-              duration: 0,
-            });          
-          };
+                message: 'Change Password Successfully.',
+                description:
+                    'Done. You will be redirected to login screen now.',
+                duration: 0,
+            });
+        };
         return (
             <Form onSubmit={this.handleSubmit} className="login-form">
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
@@ -32,24 +32,24 @@ class Change extends React.Component {
                 </div>
                 <h1>Change Password</h1>
                 <Form.Item>
-                <div className="label">Your email</div>
-                {getFieldDecorator('username', {
-                    rules: [
-                        {
-                            required: true,
-                            message: 'Please input your email'
-                        },
-                        {
-                            type: 'email',
-                            message: 'Invalid email'
-                        }
-                    ],
-                })(
-                    <Input
-                        prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    />,
-                )}
-            </Form.Item>
+                    <div className="label">Your email</div>
+                    {getFieldDecorator('username', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Please input your email'
+                            },
+                            {
+                                type: 'email',
+                                message: 'Invalid email'
+                            }
+                        ],
+                    })(
+                        <Input
+                            prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        />,
+                    )}
+                </Form.Item>
                 <Form.Item>
                     <Button type="primary" htmlType="submit" className="login-form-button" onClick={() => openNotification('success')}>
                         Change Password
