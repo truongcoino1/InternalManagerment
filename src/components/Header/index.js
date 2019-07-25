19a9a4826f556ef8c8f2e2c6ff9dd5866e049602
miphamj <hami9696@gmail.com>
2019-07-25 09:17:55 +0700

    add header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,23 +2,58 @@ import React from 'react';
 
 import 'antd/dist/antd.css';
 import './index.scss';
-import headerLogo from '../../assets/itleadpro_logo_square.png';
+import headerLogo from '../../assets/itleadpro.png';
 
-import { Row, Col } from 'antd';
+import { Row, Col, Icon, Menu } from 'antd';
 
-class Header extends React.Component{
+const { SubMenu } = Menu;
+export class Header extends React.Component{
     render(){
         return(
-            <Row>
-                <Col md={12}>
-                    <img src={headerLogo} />
-                </Col>
-                <Col md={12}>
+            <div className='header-bar'>
+                <Row className='format'>
+                    <Col md={12} className='logo'>
+                        <img src={headerLogo} />
+                    </Col>
+                    <Col md={12} className='left-menu'>
+                        <Menu mode="horizontal">
+                            <SubMenu
+                                title={
+                                    <span className="submenu-title-wrapper">
+                                        <Icon type="calendar" />
+                                        </span>
+                                }
+                            >
+                                <Menu.Item>Attendance</Menu.Item>
+                                <Menu.Item>Create a day-off</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                title={
+                                    <span className="submenu-title-wrapper">
+                                        <Icon type="mail" />
+                                        </span>
+                                }
+                            >
 
-                </Col>
-            </Row>
+                            </SubMenu>
+                            <SubMenu
+                                title={
+                                    <span className="submenu-title-wrapper">
+                                        <Icon type="user" />
+                                        </span>
+                                }
+                            >
+                                <Menu.Item>
+                                    <a href='/profile'>Profile</a>
+                                </Menu.Item>
+                                <Menu.Item>Days-off list</Menu.Item>
+                                <Menu.Item>Log out</Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Col>
+                </Row>
+            </div>
+            
         );
     }
 }
-
-export default Header;
--- src/containers/root.js
+++ src/containers/root.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { Home } from '../containers/authen/home'
+import { Profile } from '../containers/authen/profile'
 import  Login  from '../containers/guest/login'
 import Change from './guest/change_password'
 import Find from './guest/forgot_password'
@@ -19,11 +20,12 @@ export default class Root extends Component {
       <Provider store={persistor}>
         <Router history={browserHistory}>
           <Switch>
-            <PrivateRoute exact path="/" component={Home} />
+            {/* <PrivateRoute exact path="/" component={Home} /> */}
             <Route path="/login" component={Login} />
             <Route path="/changepassword" component={Change}/>
             <Route path="/findpassword" component={Find}/>
             <Route path="/home" component={Home} />
+            <Route path="/profile" component={Profile} />
           </Switch>
         </Router>
       </Provider>
