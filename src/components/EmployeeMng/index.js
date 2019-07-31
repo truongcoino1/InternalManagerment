d6c4053f3ee79137c3f85ee34999a7b3b31a8f5e
miphamj <hami9696@gmail.com>
2019-07-31 17:19:57 +0700

    edit status color

--- src/components/EmployeeMng/index.js
+++ src/components/EmployeeMng/index.js
@@ -16,16 +16,14 @@ const columns = [
     {
         title: 'Full name',
         dataIndex: 'fullName',
-        defaultSortOrder: 'descend',
-        sorter: (a, b) => a.fullName.length - b.fullName.length,
+        sorter: (a, b) => b.fullName.toLowerCase() > a.fullName.toLowerCase() ? 1 : -1,
     },
     {
         title: 'Date of birth',
         dataIndex: 'dateOfBirth',
-        defaultSortOrder: 'descend',
         sorter: (a, b) => {
-            a = new Date(a).getTime();
-            b = new Date(b).getTime();
+            a = new Date(a.dateOfBirth);
+            b = new Date(b.dateOfBirth);
             return b > a ? 1 : -1;
         }
     },
@@ -45,7 +43,7 @@ const columns = [
         title: 'Status',
         dataIndex: 'status',
         render: status => <div>
-            {status ? 'Active' : 'Deactive'}
+            {status ? <span style={{color: 'green'}}>Activate</span> : <span style={{color: 'red'}}>Deactivate</span>}
         </div>,
     }
 ]
@@ -56,14 +54,14 @@ export class EmployeeMng extends React.Component {
         this.state = {
             loading: false,
             data: [
-                { key: '1', id: 'EM001', fullName: 'Ahihi Ahehe', dateOfBirth: '01/01/2019', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
+                { key: '1', id: 'EM001', fullName: 'Ahihi ahehe', dateOfBirth: '01/01/2019', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
                 { key: '2', id: 'EM002', fullName: 'Ahihi a', dateOfBirth: '01/01/2019', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
-                { key: '3', id: 'EM003', fullName: 'Ahihi b', dateOfBirth: '01/01/2019', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: false, },
-                { key: '4', id: 'EM004', fullName: 'Ahihi w', dateOfBirth: '01/01/2019', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
-                { key: '5', id: 'EM005', fullName: 'Ahihi d', dateOfBirth: '01/01/2019', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
-                { key: '6', id: 'EM006', fullName: 'Ahihi e', dateOfBirth: '01/01/2019', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
-                { key: '7', id: 'EM007', fullName: 'Ahihi m', dateOfBirth: '01/01/2019', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: false, },
-                { key: '8', id: 'EM008', fullName: 'Ahihi g', dateOfBirth: '01/01/2019', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
+                { key: '3', id: 'EM003', fullName: 'Ahihi b', dateOfBirth: '02/01/2016', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: false, },
+                { key: '4', id: 'EM004', fullName: 'Ahihi w', dateOfBirth: '01/01/2017', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
+                { key: '5', id: 'EM005', fullName: 'Ahihi d', dateOfBirth: '03/01/2018', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
+                { key: '6', id: 'EM006', fullName: 'Ahihi e', dateOfBirth: '01/01/2013', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
+                { key: '7', id: 'EM007', fullName: 'Ahihi m', dateOfBirth: '01/01/2011', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: false, },
+                { key: '8', id: 'EM008', fullName: 'Ahihi g', dateOfBirth: '05/01/2015', phoneNumber: '0978456321', email: 'ahihi@itleadpro.vn', position: 'Developer', status: true, },
             ],
         }
     };
